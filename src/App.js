1de7d240dd473a29aeb25cf826062fafe6f507f8
Mateusz Stawowski <mathias.najlepszy@gmail.com>
2019-12-05 11:12:59 +0100

    Choose Ant Design library as a source of primary React components.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'antd/dist/antd.css';
+import { DatePicker } from 'antd';
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +12,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <DatePicker />
         </p>
         <a
           className="App-link"
