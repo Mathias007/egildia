28b6df53948b690d3858f3708d55f9b8e55e6ad5
Mateusz Stawowski <mathias.najlepszy@gmail.com>
2019-12-08 23:02:26 +0100

    + Install react-image library; + add images to K&M Buildings component: - buildings - map(), - cost - render; + start adding the other images to the component (using regExp & map()); + fix technical problems (ex. with MongoDB).

--- src/pages/KnightsBuildingsContent.js
+++ src/pages/KnightsBuildingsContent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import { connect } from "react-redux";
 
 import { knights } from "../_store/_actions";
@@ -8,58 +7,12 @@ import { Layout, Table } from 'antd';
 
 import BreadcrumbComponent from './components/BreadcrumbComponent';
 
-const { Content } = Layout;
-
-const columns = [
-  {
-    title: 'Nazwa',
-    dataIndex: 'name',
-    align: 'center',
-    render: text => <a href="#">{text}</a>,
-  },
-  {
-    title: 'Koszt',
-    dataIndex: 'cost',
-    align: 'center'
-  },
-  {
-    title: 'Pola',
-    dataIndex: 'fields',
-    align: 'center'
-  },
-  {
-    title: 'Wytrzymałość',
-    dataIndex: 'durability',
-    align: 'center'
-  },
-  {
-    title: 'Działanie',
-    dataIndex: 'description',
-    render: (description) => (
-      <span>
-        {description.map(fragment => {
-          if (fragment) {
-            return <p key={fragment}>{fragment}</p>
-          } else return null
-        })}
-      </span>
-    ),
-  },
-  {
-    title: 'Pracownik',
-    dataIndex: 'worker',
-    align: 'center',
-  }, {
-    title: 'Grafika',
-    dataIndex: 'image',
-    align: 'center',
-  },
-];
-
-let data = [];
+import Img from 'react-image'
 
 class KnightsBuildingsContent extends Component {
   state = {
+    tableColumns: [],
+    tableData: [],
   }
 
   componentDidMount() {
@@ -71,19 +24,19 @@ class KnightsBuildingsContent extends Component {
 
     if (buildings) {
       return buildings.map((building, index) => {
-        const { _id, nazwa, koszt, pola, wytrzymalosc, dzialanie, pracownik } = building;
+        const { _id, nazwa, koszt, pola, wytrzymalosc, dzialanie, pracownik, grafika } = building;
         const { deski, kamienie } = koszt;
         const { opis, potrzeby, produkcja } = dzialanie;
         return (
           {
             key: _id,
             name: nazwa,
-            cost: `${deski} <deska> ${kamienie} <kamien>`,
+            cost: [deski, kamienie],
             fields: pola,
             durability: wytrzymalosc,
             description: [opis, potrzeby, produkcja],
             worker: pracownik,
-            image: 'grafika',
+            image: grafika,
           }
         );
       });
@@ -91,8 +44,73 @@ class KnightsBuildingsContent extends Component {
   }
 
   render() {
+    const { Content } = Layout;
 
-    data = this.renderBuildings();
+    let { tableColumns, tableData } = this.state;
+
+    tableData = this.renderBuildings();
+
+    tableColumns = [{
+      title: 'Nazwa',
+      dataIndex: 'name',
+      align: 'center',
+      render: name => <h3><strong>{name}</strong></h3>,
+    },
+    {
+      title: 'Koszt',
+      dataIndex: 'cost',
+      align: 'left',
+      render: (cost) => {
+        return (
+          <p>{cost[0]}<Img src={require(`../img/knights/surowce/deska.png`)} />
+            <br />
+            {cost[1]}<Img src={require(`../img/knights/surowce/kamien.png`)} /></p>
+        )
+      }
+    },
+    {
+      title: 'Pola',
+      dataIndex: 'fields',
+      align: 'center'
+    },
+    {
+      title: 'Wytrzymałość',
+      dataIndex: 'durability',
+      align: 'center'
+    },
+    {
+      title: 'Działanie',
+      dataIndex: 'description',
+      render: (description) => {
+        return (
+          <span>
+            {description.map(fragment => {
+              if (fragment) {
+                var regex = /\<(.*?)\>/g;
+
+                var string = '<GeeksForGeeks is a CS portal>';
+                // var newstring = fragment.replace(re, '**');
+                var newstring = fragment.replace(regex, fragment.match(regex));
+                console.log(fragment.match(regex));
+                return <p key={fragment}>{newstring}</p>
+              } else return null
+            })}
+          </span>
+        )
+      },
+    },
+    {
+      title: 'Pracownik',
+      dataIndex: 'worker',
+      align: 'center',
+    }, {
+      title: 'Grafika',
+      dataIndex: 'image',
+      align: 'center',
+      render: (image) => <Img width="120px" height="100px" src={require(`../img/knights/budynki/${image}.bmp`)} />
+    }
+
+    ]
 
     return (
       <Layout style={{ padding: '0 24px 24px' }}>
@@ -108,7 +126,7 @@ class KnightsBuildingsContent extends Component {
           <h1>Knights and Merchants</h1>
           <h2>Budynki</h2>
 
-          <Table dataSource={data} columns={columns} />
+          <Table dataSource={tableData} columns={tableColumns} />
         </Content>
       </Layout>
     );
