b21ba61be3d0c9b9e37cca8c77dffe99ed796d86
Mateusz Stawowski <mathias.najlepszy@gmail.com>
2019-12-09 20:08:08 +0100

    General refactoring, destructurisation and other technical changes in component's code.

--- src/pages/KnightsBuildingsContent.js
+++ src/pages/KnightsBuildingsContent.js
@@ -1,16 +1,64 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-
 import { knights } from "../_store/_actions";
 
 import { Layout, Table } from 'antd';
-
 import BreadcrumbComponent from './components/BreadcrumbComponent';
-
 import Img from 'react-image'
+
+const textBetweenTagsRegEx = /[^<\}]+(?=>)/g;
+
+const componentStyles = {
+  content: {
+    background: '#fff',
+    padding: 24,
+    margin: 0,
+    minHeight: 280,
+  },
+  layout: { padding: '0 24px 24px' }
+}
+
+const componentClassnames = {
+  images: {
+    building: "knights-image-building",
+    icon: "knights-image-icon",
+    material: "knights-image-material",
+    unit: "knights-image-unit"
+  },
+  content: 'knights-buildings-content',
+  layout: 'knights-buildings-layout',
+  table: 'knights-buildings-table'
+}
 
 class KnightsBuildingsContent extends Component {
   state = {
+    imgPath: {
+      general: 'img',
+      section: {
+        knights: 'knights',
+      },
+      dir: {
+        buildings: 'budynki',
+        icons: 'ikony',
+        materials: 'surowce',
+        units: 'jednostki'
+      },
+      format: {
+        bmp: 'bmp',
+        png: 'png',
+        PNG: 'PNG',
+      }
+    },
+    columnsStructure: {
+      col_name: { title: 'Nazwa', dataIndex: 'name', align: 'center' },
+      col_cost: { title: 'Koszt', dataIndex: 'cost', align: 'left' },
+      col_fields: { title: 'Pola', dataIndex: 'fields', align: 'center' },
+      col_durability: { title: 'Wytrzymałość', dataIndex: 'durability', align: 'center' },
+      col_description: { title: 'Działanie', dataIndex: 'description' },
+      col_worker: { title: 'Pracownik', dataIndex: 'worker', align: 'center' },
+      col_image: { title: 'Grafika', dataIndex: 'image', align: 'center' },
+    },
+    descriptionSubHeaders: ['Opis: ', 'Potrzebuje: ', 'Produkuje: '],
     tableColumns: [],
     tableData: [],
   }
@@ -30,7 +78,7 @@ class KnightsBuildingsContent extends Component {
         return (
           {
             key: _id,
-            name: nazwa,
+            name: [nazwa, grafika],
             cost: [deski, kamienie],
             fields: pola,
             durability: wytrzymalosc,
@@ -48,95 +96,76 @@ class KnightsBuildingsContent extends Component {
 
     let { tableColumns, tableData } = this.state;
 
+    const { general, section, dir, format } = this.state.imgPath;
+
     tableData = this.renderBuildings();
 
-    tableColumns = [{
-      title: 'Nazwa',
-      dataIndex: 'name',
-      align: 'center',
-      render: name => <h3><strong>{name}</strong></h3>,
-    },
-    {
-      title: 'Koszt',
-      dataIndex: 'cost',
-      align: 'left',
-      render: (cost) => {
-        return (
-          <p>{cost[0]}<Img src={require(`../img/knights/surowce/deska.png`)} />
-            <br />
-            {cost[1]}<Img src={require(`../img/knights/surowce/kamien.png`)} /></p>
-        )
-      }
-    },
-    {
-      title: 'Pola',
-      dataIndex: 'fields',
-      align: 'center'
-    },
-    {
-      title: 'Wytrzymałość',
-      dataIndex: 'durability',
-      align: 'center'
-    },
-    {
-      title: 'Działanie',
-      dataIndex: 'description',
-      render: (description) => {
-        const descriptionSubHeaders = ['Opis: ', 'Potrzebuje: ', 'Produkuje: ']
-        return (
-          <span>
-            {description.map((fragment, index) => {
-              if (fragment) {
-                const regexMaterialsSchema = /[^<\}]+(?=>)/g;
+    const { col_name, col_cost, col_fields, col_durability, col_description, col_worker, col_image } = this.state.columnsStructure;
 
-                let materialElements = fragment.match(regexMaterialsSchema);
-                // console.log(materialElements);
-
-                return (
-                  <p key={fragment}><strong>{descriptionSubHeaders[index]}</strong>
-                    {materialElements ? materialElements.map((element, index) => <Img src={require(`../img/knights/surowce/${element}.png`)} />) : fragment}
-                  </p>
-                )
-              } else return null
-            })}
-          </span>
-        )
+    tableColumns = [
+      {
+        title: col_name.title, dataIndex: col_name.dataIndex, align: col_name.align,
+        render: (name) => (
+          <>
+            <h3><strong>{name[0]}</strong></h3>
+            <Img className={componentClassnames.images.icon} src={require(`../${general}/${section.knights}/${dir.icons}/${name[1]}.${format.png}`)} />
+          </>),
       },
-    },
-    {
-      title: 'Pracownik',
-      dataIndex: 'worker',
-      align: 'center',
-      render: (worker) => {
-        const regexWorkersSchema = /[^<\}]+(?=>)/g;
-        let workersElements = worker.match(regexWorkersSchema)
-        console.log(workersElements);
-        return (workersElements ? <Img src={require(`../img/knights/jednostki/${workersElements}.PNG`)} /> : null)
+      {
+        title: col_cost.title, dataIndex: col_cost.dataIndex, align: col_cost.align,
+        render: (cost) => {
+          return (
+            <p>{cost[0]} <Img className={componentClassnames.images.material} src={require(`../${general}/${section.knights}/${dir.materials}/deska.${format.png}`)} />
+              <br />
+              {cost[1]}<Img className={componentClassnames.images.material} src={require(`../${general}/${section.knights}/${dir.materials}/kamien.${format.png}`)} /></p>
+          )
+        }
+      },
+      { title: col_fields.title, dataIndex: col_fields.dataIndex, align: col_fields.align, },
+      { title: col_durability.title, dataIndex: col_durability.dataIndex, align: col_durability.align, },
+      {
+        title: col_description.title, dataIndex: col_description.dataIndex,
+        render: (description) => {
+          const { descriptionSubHeaders } = this.state;
+          return (
+            <span>
+              {description.map((fragment, index) => {
+                if (fragment) {
+                  let materialElements = fragment.match(textBetweenTagsRegEx);
+                  return (
+                    <p key={fragment}><strong>{descriptionSubHeaders[index]}</strong>
+                      {materialElements ? materialElements.map((element, index) => <Img className={componentClassnames.images.material} src={require(`../${general}/${section.knights}/${dir.materials}/${element}.${format.png}`)} />) : fragment}
+                    </p>
+                  )
+                } else return null
+              })}
+            </span>
+          )
+        },
+      },
+      {
+        title: col_worker.title, dataIndex: col_worker.dataIndex, align: col_worker.align,
+        render: (worker) => {
+          let workersElements = worker.match(textBetweenTagsRegEx)
+          console.log(workersElements);
+          return (workersElements ? <Img className={componentClassnames.images.unit} src={require(`../${general}/${section.knights}/${dir.units}/${workersElements}.${format.PNG}`)} /> : null)
+        }
+      }, {
+        title: col_image.title, dataIndex: col_image.dataIndex, align: col_image.align,
+        render: (image) => <Img className={componentClassnames.images.building} width="120px" height="100px" src={require(`../${general}/${section.knights}/${dir.buildings}/${image}.${format.bmp}`)} />
       }
-    }, {
-      title: 'Grafika',
-      dataIndex: 'image',
-      align: 'center',
-      render: (image) => <Img width="120px" height="100px" src={require(`../img/knights/budynki/${image}.bmp`)} />
-    }
-
     ]
 
     return (
-      <Layout style={{ padding: '0 24px 24px' }}>
+      <Layout className={componentClassnames.layout} style={componentStyles.layout}>
         <BreadcrumbComponent />
-        <Content
-          style={{
-            background: '#fff',
-            padding: 24,
-            margin: 0,
-            minHeight: 280,
-          }}
+        <Content className={componentClassnames.images.content}
+          style={componentStyles.content}
         >
           <h1>Knights and Merchants</h1>
           <h2>Budynki</h2>
 
-          <Table dataSource={tableData} columns={tableColumns} />
+          <Table className={componentClassnames.images.table} dataSource={tableData} columns={tableColumns} />
         </Content>
       </Layout>
     );
