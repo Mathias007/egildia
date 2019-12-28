7474a47189b16cdbd049a031da7590ec4e6a1ddf
Mathias007 <mathias.najlepszy@gmail.com>
2019-12-28 19:49:45 +0100

    Extend AvatarGlobalComponent & add some styles.

--- frontend/src/pages/components/AvatarGlobalComponent.js
+++ frontend/src/pages/components/AvatarGlobalComponent.js
@@ -5,38 +5,45 @@ import { Link } from "react-router-dom";
 
 import { auth } from "../../_store/_actions";
 
-import { Avatar, Button, Typography } from "antd";
-
-const {Title} = Typography;
+import { Avatar, Button } from "antd";
 
 class AvatarGlobalComponent extends Component {
     render() {
         return (
-            <div className="avatar-component">
+            <>
                 {this.props.name ? (
-                    <>
-                        <Avatar
-                            style={{
-                                backgroundColor: "#f56a00",
-                                verticalAlign: "middle"
-                            }}
-                            size="large"
-                        >
-                            {this.props.name.charAt(0)}
-                        </Avatar>
-                        <Title level={3}>{this.props.name}</Title>
-                        <Button
-                            type="primary"
-                            size="small"
-                            style={{ marginLeft: 16, verticalAlign: "middle" }}
-                            onClick={this.props.logout}
-                        >
-                            Wyloguj się
-                        </Button>
-                    </>
+                    <div className="avatar-component">
+                        <p className="avatar-component-username">
+                            <Avatar
+                                style={{
+                                    backgroundColor: "#f56a00",
+                                    verticalAlign: "middle"
+                                }}
+                                size="large"
+                            >
+                                {this.props.name.charAt(0)}
+                            </Avatar>
+                            <span>
+                                Witaj <strong>{this.props.name}</strong>!
+                            </span>
+                            <Button
+                                icon="logout"
+                                type="primary"
+                                size="small"
+                                style={{
+                                    marginLeft: 16,
+                                    verticalAlign: "middle"
+                                }}
+                                onClick={this.props.logout}
+                            >
+                                Wyloguj się
+                            </Button>
+                        </p>
+                    </div>
                 ) : (
                     <Link to="/login">
                         <Button
+                            icon="login"
                             type="primary"
                             size="small"
                             style={{ marginLeft: 16, verticalAlign: "middle" }}
@@ -45,7 +52,7 @@ class AvatarGlobalComponent extends Component {
                         </Button>
                     </Link>
                 )}
-            </div>
+            </>
         );
     }
 }
