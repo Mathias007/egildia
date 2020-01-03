ea0d26659cf62103482299adde73d5de63d068b7
Mathias007 <mathias.najlepszy@gmail.com>
2020-01-03 15:38:46 +0100

    Create dynamic NewsPage.

--- frontend/src/pages/NewsPage.js
+++ frontend/src/pages/NewsPage.js
@@ -1,20 +1,86 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import moment from "moment";
 
 import { news } from "../_store/_actions";
 
-import { Card } from "antd";
+import { Avatar, Card, Icon } from "antd";
+const { Meta } = Card;
 
 class NewsPage extends Component {
     componentDidMount() {
         this.props.showNewsList();
     }
 
+    renderNews() {
+        let { news } = this.props;
+
+        if (news) {
+            return news.map((singleNews, index) => {
+                const {
+                    _id,
+                    category,
+                    title,
+                    content,
+                    author,
+                    date
+                } = singleNews;
+                return (
+                    <Card
+                        key={_id}
+                        type="inner"
+                        title={title}
+                        extra={
+                            <>
+                                Opcje:
+                                <Link
+                                    to={`admin/news/edit/${_id}`}
+                                    // component={EditNewsComponent}
+                                >
+                                    {/* this.props.match.params._id */}
+                                    <Icon type="edit" />
+                                </Link>
+                                |
+                                <Link
+                                    to={`admin/news/delete/${_id}`}
+                                    // component={DeleteNewsComponent}
+                                >
+                                    {/* this.props.match.params._id */}
+                                    <Icon type="delete" />
+                                </Link>
+                            </>
+                        }
+                        actions={content.length > 100 ? [<p>[dalej]</p>] : null}
+                    >
+                        <Meta
+                            avatar={
+                                <Avatar
+                                    style={{
+                                        backgroundColor: "violet",
+                                        verticalAlign: "middle"
+                                    }}
+                                    size="large"
+                                >
+                                    {category.charAt(0) + category.charAt(1)}
+                                </Avatar>
+                            }
+                            description={`Data dodania: ${moment(date).format(
+                                "LLLL"
+                            )} | Autor: ${author}`}
+                        />
+                        <p>{content}</p>
+                    </Card>
+                );
+            });
+        }
+    }
+
     render() {
         return (
             <div>
-                <Card title="Card title">
-                    <p
+                <Card title="Aktualności">
+                    {/* <p
                         style={{
                             fontSize: 14,
                             color: "rgba(0, 0, 0, 0.85)",
@@ -23,22 +89,8 @@ class NewsPage extends Component {
                         }}
                     >
                         Group title
-                    </p>
-                    <Card
-                        type="inner"
-                        title="Inner Card title"
-                        extra={<a href="#">More</a>}
-                    >
-                        Inner Card content
-                    </Card>
-                    <Card
-                        style={{ marginTop: 16 }}
-                        type="inner"
-                        title="Inner Card title"
-                        extra={<a href="#">More</a>}
-                    >
-                        Inner Card content
-                    </Card>
+                    </p> */}
+                    {this.renderNews()}
                 </Card>
             </div>
         );
