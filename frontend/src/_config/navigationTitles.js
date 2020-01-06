af96417117a5cb2e0b97d0148fea0f27bcc5cd74
Mathias007 <mathias.najlepszy@gmail.com>
2020-01-06 23:52:13 +0100

    Add sidebar handle, refactor (nav titles in config).

---
+++ frontend/src/_config/navigationTitles.js
@@ -0,0 +1,20 @@
+const navigationTitles = {
+    GENERAL_INDEX: "Strona główna",
+    KNIGHTS_AND_MERCHANTS: "Knights and Merchants",
+    KNIGHTS_MAIN: "Wprowadzenie",
+    KNIGHTS_BUILDINGS: "Budynki",
+    KNIGHTS_UNITS: "Jednostki",
+    KNIGHTS_REVIEW: "Recenzja",
+    TZAR_BURDEN_CROWN: "Tzar: Ciężar Korony",
+    TZAR_MAIN: "Wprowadzenie",
+    TZAR_NATIONS: "Nacje",
+    TZAR_SPELLS: "Czary",
+    TZAR_TECHNOLOGIES: "Technologie",
+    TZAR_UNITS: "Jednostki",
+    ADMINISTRATION: "Administracja",
+    ADMIN_ARTICLES: "Artykuły",
+    ADMIN_NEWS: "Wpisy",
+    ADMIN_USERS: "Użytkownicy"
+};
+
+export default navigationTitles;
--- frontend/src/pages/global/SidebarComponent.js
+++ frontend/src/pages/global/SidebarComponent.js
@@ -1,48 +1,94 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import { Layout, Menu, Icon } from "antd";
+import { connect } from "react-redux";
 
 import routesPaths from "../../_config/routesPaths";
-const { GENERAL, ARTICLES, NEWS, KNIGHTS, TZAR } = routesPaths;
+import navigationTitles from "../../_config/navigationTitles";
 
+import { Layout, Menu, Icon } from "antd";
 const { SubMenu } = Menu;
 const { Sider } = Layout;
 const { Item } = Menu;
 
+const { GENERAL, ARTICLES, NEWS, KNIGHTS, TZAR } = routesPaths;
+const {
+    GENERAL_INDEX,
+    KNIGHTS_AND_MERCHANTS,
+    KNIGHTS_MAIN,
+    KNIGHTS_BUILDINGS,
+    KNIGHTS_UNITS,
+    KNIGHTS_REVIEW,
+    TZAR_BURDEN_CROWN,
+    TZAR_MAIN,
+    TZAR_NATIONS,
+    TZAR_SPELLS,
+    TZAR_TECHNOLOGIES,
+    TZAR_UNITS,
+    ADMINISTRATION,
+    ADMIN_ARTICLES,
+    ADMIN_NEWS,
+    ADMIN_USERS
+} = navigationTitles;
+
 class SidebarComponent extends Component {
-    state = {};
+    rootSubmenuKeys = ["knights", "tzar", "admin"];
+
+    state = {
+        openKeys: ["knights"]
+    };
+
+    onOpenChange = openKeys => {
+        const latestOpenKey = openKeys.find(
+            key => this.state.openKeys.indexOf(key) === -1
+        );
+        if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+            this.setState({ openKeys });
+        } else {
+            this.setState({
+                openKeys: latestOpenKey ? [latestOpenKey] : []
+            });
+        }
+    };
+
     render() {
         return (
-            <Sider width={200} style={{ background: "#fff" }}>
+            <Sider width={230} style={{ background: "#fff" }}>
                 <Menu
                     mode="inline"
-                    defaultSelectedKeys={["1"]}
-                    defaultOpenKeys={["knights"]}
+                    openKeys={this.state.openKeys}
+                    onOpenChange={this.onOpenChange}
                     style={{ height: "100%", borderRight: 0 }}
                 >
                     <Item key="0">
-                        <NavLink to={GENERAL.INDEX}>Strona główna</NavLink>
+                        <NavLink to={GENERAL.INDEX}>
+                            <Icon type="pie-chart" />
+                            <span> {GENERAL_INDEX}</span>
+                        </NavLink>
                     </Item>
                     <SubMenu
                         key="knights"
                         title={
                             <span>
                                 <Icon type="skin" />
-                                Knights and Merchants
+                                {KNIGHTS_AND_MERCHANTS}
                             </span>
                         }
                     >
                         <Item key="1">
-                            <NavLink to={KNIGHTS.MAIN}>Wprowadzenie</NavLink>
+                            <NavLink to={KNIGHTS.MAIN}>{KNIGHTS_MAIN}</NavLink>
                         </Item>
                         <Item key="2">
-                            <NavLink to={KNIGHTS.BUILDINGS}>Budynki</NavLink>
+                            <NavLink to={KNIGHTS.BUILDINGS}>
+                                {KNIGHTS_BUILDINGS}
+                            </NavLink>
                         </Item>
                         <Item key="3">
-                            <NavLink to={KNIGHTS.UNITS}>Jednostki</NavLink>
+                            <NavLink to={KNIGHTS.UNITS}>
+                                {KNIGHTS_UNITS}
+                            </NavLink>
                         </Item>
                         <Item key="4">
-                            <NavLink to="#">Recenzja</NavLink>
+                            <NavLink to="#">{KNIGHTS_REVIEW}</NavLink>
                         </Item>
                     </SubMenu>
                     <SubMenu
@@ -50,51 +96,64 @@ class SidebarComponent extends Component {
                         title={
                             <span>
                                 <Icon type="crown" />
-                                Tzar: Ciężar Korony
+                                {TZAR_BURDEN_CROWN}
                             </span>
                         }
                     >
                         <Item key="5">
-                            <NavLink to={TZAR.MAIN}>Wprowadzenie</NavLink>
+                            <NavLink to={TZAR.MAIN}>{TZAR_MAIN}</NavLink>
                         </Item>
                         <Item key="6">
-                            <NavLink to={TZAR.NATIONS}>Budynki</NavLink>
+                            <NavLink to={TZAR.NATIONS}>{TZAR_NATIONS}</NavLink>
                         </Item>
                         <Item key="7">
-                            <NavLink to={TZAR.UNITS}>Jednostki</NavLink>
+                            <NavLink to={TZAR.UNITS}>{TZAR_UNITS}</NavLink>
                         </Item>
                         <Item key="8">
-                            <NavLink to={TZAR.SPELLS}>Magia</NavLink>
+                            <NavLink to={TZAR.SPELLS}>{TZAR_SPELLS}</NavLink>
                         </Item>
                         <Item key="9">
                             <NavLink to={TZAR.TECHNOLOGIES}>
-                                Technologie
+                                {TZAR_TECHNOLOGIES}
                             </NavLink>
                         </Item>
                     </SubMenu>
-                    <SubMenu
-                        key="admin"
-                        title={
-                            <span>
-                                <Icon type="setting" />
-                                Administracja
-                            </span>
-                        }
-                    >
-                        <Item key="10">
-                            <NavLink to={ARTICLES.MAIN}>Artykuły</NavLink>
-                        </Item>
-                        <Item key="11">
-                            <NavLink to={NEWS.MAIN}>Wpisy</NavLink>
-                        </Item>
-                        <Item key="12">
-                            <NavLink to="#">Użytkownicy</NavLink>
-                        </Item>
-                    </SubMenu>
+
+                    {this.props.isAuthenticated ? (
+                        <SubMenu
+                            key="admin"
+                            title={
+                                <span>
+                                    <Icon type="setting" />
+                                    {ADMINISTRATION}
+                                </span>
+                            }
+                        >
+                            <Item key="10">
+                                <NavLink to={ARTICLES.MAIN}>
+                                    {ADMIN_ARTICLES}
+                                </NavLink>
+                            </Item>
+                            <Item key="11">
+                                <NavLink to={NEWS.MAIN}>{ADMIN_NEWS}</NavLink>
+                            </Item>
+                            <Item key="12">
+                                <NavLink to="#">{ADMIN_USERS}</NavLink>
+                            </Item>
+                        </SubMenu>
+                    ) : null}
                 </Menu>
             </Sider>
         );
     }
 }
 
-export default SidebarComponent;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.isAuthenticated
+    };
+};
+
+const mapDispatchToProps = dispatch => {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SidebarComponent);
