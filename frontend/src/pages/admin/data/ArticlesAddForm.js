5b1e4f9098ceab8d0e38dc47205cf40e3e77064c
Mathias007 <mathias.najlepszy@gmail.com>
2020-01-16 23:27:01 +0100

    implement SingleFormElement into ArticlesAddForm.

--- frontend/src/pages/admin/data/ArticlesAddForm.js
+++ frontend/src/pages/admin/data/ArticlesAddForm.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import locale from "antd/es/date-picker/locale/pl_PL";
 import { connect } from "react-redux";
 
 import { articles } from "../../../_store/_actions";
 
-import { Button, DatePicker, Form, Icon, Input, Layout, Tooltip } from "antd";
+import SingleFormElement from "../../components/SingleFormElement";
+
+import { Button, Form, Layout } from "antd";
 import styles from "../../../styles/styles";
 
 const { Item } = Form;
-const { TextArea } = Input;
 const { Content } = Layout;
 
 const buttonData = {
@@ -45,146 +45,61 @@ class ArticlesAddForm extends Component {
         return (
             <Content style={styles.content}>
                 <Form onSubmit={this.handleSubmit} id="add-article-form">
-                    <Item label="Klucz identyfikacyjny artykułu">
-                        {getFieldDecorator("allocationKey", {
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Podaj klucz artykułu!"
-                                }
-                            ]
-                        })(
-                            <Input
-                                prefix={
-                                    <Icon type="key" style={styles.inputIcon} />
-                                }
-                                suffix={
-                                    <Tooltip title="Klucz artykułu to nadany przez autora identyfikator tekstowy, który umożliwia wygenerowanie treści w odpowiednim miejscu w serwisie.">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Wpisz unikalny klucz artykułu"
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        label="Klucz identyfikacyjny artykułu"
+                        fieldName="allocationKey"
+                        inputIcon="key"
+                        message="Podaj klucz artykułu!"
+                        placeholder="Wpisz unikalny klucz artykułu"
+                        required
+                        tooltip="Klucz artykułu to nadany przez autora identyfikator tekstowy, który umożliwia wygenerowanie treści w odpowiednim miejscu w serwisie."
+                    />
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        label="Tytuł artykułu"
+                        fieldName="title"
+                        inputIcon="flag"
+                        message="Wpisz tytuł artykułu!"
+                        placeholder="Nadaj artykułowi tytuł"
+                        required
+                        tooltip="Tytuł artykułu, wyświetlany jako nagłówek podstrony zawierającej."
+                    />
 
-                    <Item label="Tytuł artykułu">
-                        {getFieldDecorator("title", {
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Wpisz tytuł artykułu!"
-                                }
-                            ]
-                        })(
-                            <Input
-                                prefix={
-                                    <Icon
-                                        type="flag"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                suffix={
-                                    <Tooltip title="Tytuł artykułu, wyświetlany jako nagłówek podstrony zawierającej.">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Nadaj artykułowi tytuł"
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        fieldName="content"
+                        fieldType="text-area"
+                        label="Zawartość artykułu"
+                        message="Wpisz zawartość artykułu!"
+                        placeholder="Daj ponieść się ekspresji..."
+                        required
+                        rows={8}
+                    />
 
-                    <Item label="Zawartość artykułu">
-                        {getFieldDecorator("content", {
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Wpisz zawartość artykułu!"
-                                }
-                            ]
-                        })(
-                            <TextArea
-                                rows={4}
-                                prefix={
-                                    <Icon
-                                        type="read"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                suffix={
-                                    <Tooltip title="Zawartość artykułu, czyli tekst umieszczany jako content strony">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Daj ponieść się ekspresji..."
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        fieldName="date"
+                        fieldType="date"
+                        label="Data dodania lub utworzenia (pole nieobowiązkowe)"
+                        inputIcon="calendar"
+                        message="content"
+                        placeholder="Data utworzenia (domyślnie wygenerowana zostanie aktualna)"
+                        required={false}
+                        rows={8}
+                    />
 
-                    <Item label="Data dodania lub utworzenia (pole nieobowiązkowe)">
-                        {getFieldDecorator("date", {
-                            rules: [
-                                {
-                                    type: "object",
-                                    required: false
-                                }
-                            ]
-                        })(
-                            <DatePicker
-                                locale={locale}
-                                style={styles.datePicker}
-                                suffixIcon={
-                                    <Icon
-                                        type="calendar"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                showTime
-                                showToday
-                                format="LLLL"
-                                placeholder="Data utworzenia (domyślnie wygenerowana zostanie aktualna)"
-                            />
-                        )}
-                    </Item>
-
-                    <Item label="Autor artykułu">
-                        {getFieldDecorator("author", {
-                            initialValue: this.props.name,
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Podaj autora artykułu!"
-                                }
-                            ]
-                        })(
-                            <Input
-                                prefix={
-                                    <Icon
-                                        type="crown"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                suffix={
-                                    <Tooltip title="Wpisz nazwę użytkownika, który jest autorem artykułu. Domyślnie jest nim zalogowany użytkownik.">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Podaj autora artykułu"
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        label="Autor artykułu"
+                        fieldName="author"
+                        initialValue={this.props.name}
+                        inputIcon="crown"
+                        message="Podaj autora artykułu!"
+                        placeholder="Podaj autora artykułu"
+                        required
+                        tooltip="Wpisz nazwę użytkownika, który jest autorem artykułu. Domyślnie jest nim zalogowany użytkownik."
+                    />
 
                     <Item className="btn-wrap">
                         <Button icon={icon} type={type} htmlType={htmlType}>
