bc531f265678bb9052d8a91c5966c04b80989d2f
Mathias007 <mathias.najlepszy@gmail.com>
2020-01-17 14:06:32 +0100

    Extend SingleFormElement for password, select & email types.

--- frontend/src/pages/components/SingleFormElement.js
+++ frontend/src/pages/components/SingleFormElement.js
@@ -2,10 +2,11 @@ import React from "react";
 import locale from "antd/es/date-picker/locale/pl_PL";
 
 import styles from "../../styles/styles";
-import { DatePicker, Form, Icon, Input, Tooltip } from "antd";
+import { DatePicker, Form, Icon, Select, Input, Tooltip } from "antd";
 
 const { Item } = Form;
-const { TextArea } = Input;
+const { TextArea, Password } = Input;
+const { Option } = Select;
 
 export default function SingleFormElement(props) {
     const {
@@ -16,6 +17,9 @@ export default function SingleFormElement(props) {
         initialValue,
         inputIcon,
         message,
+        minLength,
+        options,
+        pattern,
         placeholder,
         required,
         rows,
@@ -61,13 +65,70 @@ export default function SingleFormElement(props) {
                 )}
             </Item>
         );
-    else
+    else if (fieldType === "password")
+        return (
+            <Item label={label}>
+                {getFieldDecorator(fieldName, {
+                    rules: [
+                        {
+                            required,
+                            message
+                        },
+                        { min: minLength, message: "Hasło za krótkie!" },
+                        {
+                            pattern,
+                            message: "Hasło nie spełnia wymagań!"
+                        }
+                    ]
+                })(
+                    <Password
+                        prefix={
+                            <Icon type={inputIcon} style={styles.inputIcon} />
+                        }
+                        suffix={
+                            <Tooltip title={tooltip}>
+                                <Icon
+                                    type={styles.tooltipIcon.type}
+                                    style={styles.tooltipIcon}
+                                />
+                            </Tooltip>
+                        }
+                        placeholder={placeholder}
+                    />
+                )}
+            </Item>
+        );
+    else if (fieldType === "select") {
         return (
             <Item label={label}>
                 {getFieldDecorator(fieldName, {
                     initialValue,
                     rules: [
                         {
+                            required
+                        }
+                    ]
+                })(
+                    <Select>
+                        {options.map((option, index) => {
+                            return (
+                                <Option key={option} value={option}>
+                                    {option}
+                                </Option>
+                            );
+                        })}
+                    </Select>
+                )}
+            </Item>
+        );
+    } else
+        return (
+            <Item label={label}>
+                {getFieldDecorator(fieldName, {
+                    initialValue,
+                    rules: [
+                        {
+                            type: fieldType,
                             required,
                             message
                         }
