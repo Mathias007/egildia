c69687f2ae6eda2b4ffc7c1a22100b922870c4ec
Mathias007 <mathias.najlepszy@gmail.com>
2020-01-17 11:59:28 +0100

    Implement general components into NewsAddForm.

--- frontend/src/pages/admin/data/NewsAddForm.js
+++ frontend/src/pages/admin/data/NewsAddForm.js
@@ -1,22 +1,16 @@
 import React, { Component } from "react";
-import locale from "antd/es/date-picker/locale/pl_PL";
 import { connect } from "react-redux";
 
 import { news } from "../../../_store/_actions";
 
-import { Button, DatePicker, Form, Icon, Input, Layout, Tooltip } from "antd";
+import SingleFormElement from "../../components/SingleFormElement";
+import ButtonComponent from "../../components/ButtonComponent";
+import ErrorMessageComponent from "../../components/ErrorMessageComponent";
+
+import { Form, Layout } from "antd";
 import styles from "../../../styles/styles";
 
-const { Item } = Form;
-const { TextArea } = Input;
 const { Content } = Layout;
-
-const buttonData = {
-    icon: "file-add",
-    type: "primary",
-    htmlType: "submit",
-    text: "Dodaj wpis"
-};
 
 class NewsAddForm extends Component {
     handleSubmit = e => {
@@ -41,160 +35,73 @@ class NewsAddForm extends Component {
 
     render() {
         const { getFieldDecorator } = this.props.form;
-        const { icon, type, htmlType, text } = buttonData;
         return (
             <Content style={styles.content}>
                 <Form onSubmit={this.handleSubmit} id="add-news-form">
-                    <Item label="Kategoria wpisu">
-                        {getFieldDecorator("category", {
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Określ kategorię wpisu!"
-                                }
-                            ]
-                        })(
-                            <Input
-                                prefix={
-                                    <Icon
-                                        type="folder-open"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                suffix={
-                                    <Tooltip title="Kategorie systematyzują wpisy pod kątem merytorycznym.">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Określ kategorię wpisu"
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        fieldName="category"
+                        inputIcon="folder-open"
+                        label="Kategoria wpisu"
+                        message="Określ kategorię wpisu!"
+                        placeholder="Określ kategorię wpisu"
+                        required
+                        tooltip="Kategorie systematyzują wpisy pod kątem merytorycznym."
+                    />
 
-                    <Item label="Tytuł wpisu">
-                        {getFieldDecorator("title", {
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Podaj tytuł wpisu!"
-                                }
-                            ]
-                        })(
-                            <Input
-                                prefix={
-                                    <Icon
-                                        type="flag"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                suffix={
-                                    <Tooltip title="Tytuł wpisu, wyświetlany jako nagłówek w aktualnościach i na stronie danego wpisu.">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Nadaj wpisowi tytuł"
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        fieldName="title"
+                        inputIcon="flag"
+                        label="Tytuł wpisu"
+                        message="Podaj tytuł wpisu!"
+                        placeholder="Nadaj wpisowi tytuł"
+                        required
+                        tooltip="Tytuł wpisu, wyświetlany jako nagłówek w aktualnościach i na stronie danego wpisu."
+                    />
 
-                    <Item label="Zawartość wpisu">
-                        {getFieldDecorator("content", {
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Umieść zawartość nowego wpisu!"
-                                }
-                            ]
-                        })(
-                            <TextArea
-                                rows={4}
-                                prefix={
-                                    <Icon
-                                        type="read"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                suffix={
-                                    <Tooltip title="Zawartość tekstowa wpisu.">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Daj ponieść się ekspresji..."
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        fieldName="content"
+                        fieldType="text-area"
+                        label="Zawartość wpisu"
+                        message="Umieść zawartość nowego wpisu!"
+                        placeholder="Daj ponieść się ekspresji..."
+                        required
+                        rows={8}
+                    />
 
-                    <Item label="Data dodania lub utworzenia (pole nieobowiązkowe)">
-                        {getFieldDecorator("date", {
-                            rules: [
-                                {
-                                    type: "object",
-                                    required: false
-                                }
-                            ]
-                        })(
-                            <DatePicker
-                                locale={locale}
-                                style={styles.datePicker}
-                                suffixIcon={
-                                    <Icon
-                                        type="calendar"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                showTime
-                                showToday
-                                format="LLLL"
-                                placeholder="Data utworzenia (domyślnie wygenerowana zostanie aktualna)"
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        fieldName="date"
+                        fieldType="date"
+                        label="Data dodania lub utworzenia (pole nieobowiązkowe)"
+                        inputIcon="calendar"
+                        placeholder="Data utworzenia (domyślnie wygenerowana zostanie aktualna)"
+                        required={false}
+                    />
 
-                    <Item label="Autor wpisu">
-                        {getFieldDecorator("author", {
-                            initialValue: this.props.name,
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Podaj autora wpisu!"
-                                }
-                            ]
-                        })(
-                            <Input
-                                prefix={
-                                    <Icon
-                                        type="crown"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                suffix={
-                                    <Tooltip title="Wpisz nazwę użytkownika, który jest autorem wpisu. Domyślnie jest nim zalogowany użytkownik.">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Podaj autora wpisu"
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        fieldName="author"
+                        initialValue={this.props.name}
+                        inputIcon="crown"
+                        label="Autor artykułu"
+                        message="Podaj autora wpisu!"
+                        placeholder="Podaj autora wpisu"
+                        required
+                        tooltip="Wpisz nazwę użytkownika, który jest autorem wpisu. Domyślnie jest nim zalogowany użytkownik."
+                    />
 
-                    <Item className="btn-wrap">
-                        <Button icon={icon} type={type} htmlType={htmlType}>
-                            {text}
-                        </Button>
-                    </Item>
-                    <Item>{this.props.errorMessage}</Item>
+                    <ButtonComponent
+                        htmlType="submit"
+                        icon="file-add"
+                        text="Dodaj wpis"
+                        type="primary"
+                    />
+                    <ErrorMessageComponent
+                        errorMessage={this.props.errorMessage}
+                    />
                 </Form>
             </Content>
         );
