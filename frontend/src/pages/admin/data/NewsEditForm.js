906efc9f2fdd14666ee7ff7cd37863684206fd98
Mathias007 <mathias.najlepszy@gmail.com>
2020-01-17 12:00:01 +0100

    Implement general components into NewsEditForm.

--- frontend/src/pages/admin/data/NewsEditForm.js
+++ frontend/src/pages/admin/data/NewsEditForm.js
@@ -1,22 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 
 import { news } from "../../../_store/_actions";
 
-import { Button, Divider, Form, Icon, Input, Layout, Tooltip } from "antd";
+import SingleFormElement from "../../components/SingleFormElement";
+import ButtonComponent from "../../components/ButtonComponent";
+import ErrorMessageComponent from "../../components/ErrorMessageComponent";
+
+import { Form, Layout } from "antd";
 import styles from "../../../styles/styles";
 
-const { Item } = Form;
-const { TextArea } = Input;
 const { Content } = Layout;
-
-const buttonData = {
-    icon: "edit",
-    type: "primary",
-    htmlType: "submit",
-    text: "Edytuj wpis"
-};
 
 class NewsEditForm extends Component {
     handleSubmit = e => {
@@ -43,142 +37,69 @@ class NewsEditForm extends Component {
     render() {
         const { getFieldDecorator } = this.props.form;
         const { properNews } = this.props;
-        const { icon, type, htmlType, text } = buttonData;
         return (
             <Content style={styles.content}>
                 <Form onSubmit={this.handleSubmit} id="edit-news-form">
-                    <Item label="Kategoria wpisu">
-                        {getFieldDecorator("category", {
-                            initialValue: properNews.category,
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Określ kategorię wpisu!"
-                                }
-                            ]
-                        })(
-                            <Input
-                                prefix={
-                                    <Icon type="key" style={styles.inputIcon} />
-                                }
-                                suffix={
-                                    <Tooltip title="Kategorie systematyzują wpisy pod kątem merytorycznym.">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Określ kategorię wpisu"
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        fieldName="category"
+                        initialValue={properNews.category}
+                        inputIcon="key"
+                        label="Kategoria wpisu"
+                        message="Określ kategorię wpisu!"
+                        placeholder="Określ kategorię wpisu"
+                        required
+                        tooltip="Kategorie systematyzują wpisy pod kątem merytorycznym."
+                    />
 
-                    <Item label="Tytuł wpisu">
-                        {getFieldDecorator("title", {
-                            initialValue: properNews.title,
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Wpisz tytuł wpisu!"
-                                }
-                            ]
-                        })(
-                            <Input
-                                prefix={
-                                    <Icon
-                                        type="flag"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                suffix={
-                                    <Tooltip title="Tytuł wpisu, wyświetlany jako nagłówek w aktualnościach i na stronie danego wpisu.">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Nadaj wpisowi tytuł"
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        fieldName="title"
+                        initialValue={properNews.title}
+                        inputIcon="flag"
+                        label="Tytuł wpisu"
+                        message="Wpisz tytuł wpisu!"
+                        placeholder="Nadaj wpisowi tytuł"
+                        required
+                        tooltip="Tytuł wpisu, wyświetlany jako nagłówek w aktualnościach i na stronie danego wpisu."
+                    />
 
-                    <Item label="Zawartość wpisu">
-                        {getFieldDecorator("content", {
-                            initialValue: properNews.content,
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Umieść zawartość nowego wpisu!"
-                                }
-                            ]
-                        })(
-                            <TextArea
-                                rows={4}
-                                prefix={
-                                    <Icon
-                                        type="read"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                suffix={
-                                    <Tooltip title="Zawartość tekstowa wpisu.">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Daj ponieść się ekspresji..."
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        fieldName="content"
+                        fieldType="text-area"
+                        initialValue={properNews.content}
+                        label="Zawartość wpisu"
+                        message="Umieść zawartość nowego wpisu!"
+                        placeholder="Daj ponieść się ekspresji..."
+                        required
+                        rows={8}
+                    />
 
-                    <Item label="Autor wpisu">
-                        {getFieldDecorator("author", {
-                            initialValue: properNews.author,
-                            rules: [
-                                {
-                                    required: true,
-                                    message: "Podaj autora wpisu!"
-                                }
-                            ]
-                        })(
-                            <Input
-                                prefix={
-                                    <Icon
-                                        type="crown"
-                                        style={styles.inputIcon}
-                                    />
-                                }
-                                suffix={
-                                    <Tooltip title="Wpisz nazwę użytkownika, który jest autorem wpisu. Domyślnie jest nim zalogowany użytkownik.">
-                                        <Icon
-                                            type="info-circle"
-                                            style={styles.tooltipIcon}
-                                        />
-                                    </Tooltip>
-                                }
-                                placeholder="Podaj autora wpisu"
-                            />
-                        )}
-                    </Item>
+                    <SingleFormElement
+                        getFieldDecorator={getFieldDecorator}
+                        label="Autor wpisu"
+                        fieldName="author"
+                        initialValue={properNews.author}
+                        inputIcon="crown"
+                        message="Podaj autora wpisu!"
+                        placeholder="Podaj autora wpisu"
+                        required
+                        tooltip="Wpisz nazwę użytkownika, który jest autorem wpisu. Domyślnie jest nim zalogowany użytkownik."
+                    />
 
-                    <Item className="btn-wrap">
-                        <Button icon={icon} type={type} htmlType={htmlType}>
-                            {text}
-                        </Button>
-                        <Divider
-                            type="vertical"
-                            dashed
-                            style={styles.hiddenDivider}
-                        />
-                        <Button>
-                            <Link to="/admin/news">Zrezygnuj</Link>
-                        </Button>
-                    </Item>
-                    <Item>{this.props.errorMessage}</Item>
+                    <ButtonComponent
+                        composition="double"
+                        cancelLink="/admin/news"
+                        cancelText="Zrezygnuj"
+                        htmlType="submit"
+                        icon="edit"
+                        text="Edytuj wpis"
+                        type="primary"
+                    />
+                    <ErrorMessageComponent
+                        errorMessage={this.props.errorMessage}
+                    />
                 </Form>
             </Content>
         );
