b9ac1d2cea971f2caa223e4fab2dc1741ce9be88
Mathias007 <mathias.najlepszy@gmail.com>
2020-01-18 23:47:49 +0100

    Create Tzar ColumnsData & serviceData files.

---
+++ frontend/src/pages/tzar/data/serviceData.js
@@ -0,0 +1,17 @@
+export const imgPath = {
+    general: "img",
+    section: {
+        tzar: "tzar"
+    },
+    dir: {
+        buildings: "budynki",
+        spells: "magia",
+        technologies: "technologie"
+
+    },
+    format: {
+        png: "png"
+    }
+};
+
+export const descriptionSubHeaders = ["Opis: ", "Wymagania: "];
