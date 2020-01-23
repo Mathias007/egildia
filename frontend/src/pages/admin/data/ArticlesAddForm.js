0b688f70c0fe76dc484c7ef5c146cf6fd93be62b
Mathias007 <mathias.najlepszy@gmail.com>
2020-01-23 22:35:33 +0100

    Implement Success component & status redux in ArticlesAddForm.

--- frontend/src/pages/admin/data/ArticlesAddForm.js
+++ frontend/src/pages/admin/data/ArticlesAddForm.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import { articles } from "../../../_store/_actions";
+import linksPaths from "../../../_config/linksPaths";
+import serverStatuses from "../../../_config/serverStatuses";
 import styles from "../../../styles/styles";
 
 import SingleFormElement from "../../components/SingleFormElement";
 import ButtonComponent from "../../components/ButtonComponent";
 import ErrorMessageComponent from "../../components/ErrorMessageComponent";
+import Success from "../../errors/Success";
 
 import { Form, Layout } from "antd";
 const { Content } = Layout;
+
+const { ARTICLES } = linksPaths;
+const { STATUS_OK } = serverStatuses;
 
 class ArticlesAddForm extends Component {
     handleSubmit = e => {
@@ -33,84 +39,95 @@ class ArticlesAddForm extends Component {
     };
 
     render() {
-        const { getFieldDecorator } = this.props.form;
-        return (
-            <Content style={styles.content}>
-                <Form onSubmit={this.handleSubmit} id="add-article-form">
-                    <SingleFormElement
-                        getFieldDecorator={getFieldDecorator}
-                        fieldName="allocationKey"
-                        inputIcon="key"
-                        label="Klucz identyfikacyjny artykułu"
-                        message="Podaj klucz artykułu!"
-                        placeholder="Wpisz unikalny klucz artykułu"
-                        required
-                        tooltip="Klucz artykułu to nadany przez autora identyfikator tekstowy, który umożliwia wygenerowanie treści w odpowiednim miejscu w serwisie."
-                    />
+        if (this.props.status === STATUS_OK)
+            return (
+                <Success
+                    message={this.props.errorMessage}
+                    continueFunction={this.props.cleanServerStatus}
+                    cancelLink={ARTICLES.MAIN}
+                />
+            );
+        else {
+            const { getFieldDecorator } = this.props.form;
+            return (
+                <Content style={styles.content}>
+                    <Form onSubmit={this.handleSubmit} id="add-article-form">
+                        <SingleFormElement
+                            getFieldDecorator={getFieldDecorator}
+                            fieldName="allocationKey"
+                            inputIcon="key"
+                            label="Klucz identyfikacyjny artykułu"
+                            message="Podaj klucz artykułu!"
+                            placeholder="Wpisz unikalny klucz artykułu"
+                            required
+                            tooltip="Klucz artykułu to nadany przez autora identyfikator tekstowy, który umożliwia wygenerowanie treści w odpowiednim miejscu w serwisie."
+                        />
 
-                    <SingleFormElement
-                        getFieldDecorator={getFieldDecorator}
-                        fieldName="title"
-                        inputIcon="flag"
-                        label="Tytuł artykułu"
-                        message="Wpisz tytuł artykułu!"
-                        placeholder="Nadaj artykułowi tytuł"
-                        required
-                        tooltip="Tytuł artykułu, wyświetlany jako nagłówek podstrony zawierającej."
-                    />
+                        <SingleFormElement
+                            getFieldDecorator={getFieldDecorator}
+                            fieldName="title"
+                            inputIcon="flag"
+                            label="Tytuł artykułu"
+                            message="Wpisz tytuł artykułu!"
+                            placeholder="Nadaj artykułowi tytuł"
+                            required
+                            tooltip="Tytuł artykułu, wyświetlany jako nagłówek podstrony zawierającej."
+                        />
 
-                    <SingleFormElement
-                        getFieldDecorator={getFieldDecorator}
-                        fieldName="content"
-                        fieldType="text-area"
-                        label="Zawartość artykułu"
-                        message="Wpisz zawartość artykułu!"
-                        placeholder="Daj ponieść się ekspresji..."
-                        required
-                        rows={8}
-                    />
+                        <SingleFormElement
+                            getFieldDecorator={getFieldDecorator}
+                            fieldName="content"
+                            fieldType="text-area"
+                            label="Zawartość artykułu"
+                            message="Wpisz zawartość artykułu!"
+                            placeholder="Daj ponieść się ekspresji..."
+                            required
+                            rows={8}
+                        />
 
-                    <SingleFormElement
-                        getFieldDecorator={getFieldDecorator}
-                        fieldName="date"
-                        fieldType="date"
-                        label="Data dodania lub utworzenia (pole nieobowiązkowe)"
-                        inputIcon="calendar"
-                        placeholder="Data utworzenia (domyślnie wygenerowana zostanie aktualna)"
-                        required={false}
-                    />
+                        <SingleFormElement
+                            getFieldDecorator={getFieldDecorator}
+                            fieldName="date"
+                            fieldType="date"
+                            label="Data dodania lub utworzenia (pole nieobowiązkowe)"
+                            inputIcon="calendar"
+                            placeholder="Data utworzenia (domyślnie wygenerowana zostanie aktualna)"
+                            required={false}
+                        />
 
-                    <SingleFormElement
-                        getFieldDecorator={getFieldDecorator}
-                        fieldName="author"
-                        initialValue={this.props.name}
-                        inputIcon="crown"
-                        label="Autor artykułu"
-                        message="Podaj autora artykułu!"
-                        placeholder="Podaj autora artykułu"
-                        required
-                        tooltip="Wpisz nazwę użytkownika, który jest autorem artykułu. Domyślnie jest nim zalogowany użytkownik."
-                    />
+                        <SingleFormElement
+                            getFieldDecorator={getFieldDecorator}
+                            fieldName="author"
+                            initialValue={this.props.name}
+                            inputIcon="crown"
+                            label="Autor artykułu"
+                            message="Podaj autora artykułu!"
+                            placeholder="Podaj autora artykułu"
+                            required
+                            tooltip="Wpisz nazwę użytkownika, który jest autorem artykułu. Domyślnie jest nim zalogowany użytkownik."
+                        />
 
-                    <ButtonComponent
-                        htmlType="submit"
-                        icon="file-add"
-                        text="Dodaj artykuł"
-                        type="primary"
-                    />
-                    <ErrorMessageComponent
-                        errorMessage={this.props.errorMessage}
-                    />
-                </Form>
-            </Content>
-        );
+                        <ButtonComponent
+                            htmlType="submit"
+                            icon="file-add"
+                            text="Dodaj artykuł"
+                            type="primary"
+                        />
+                        <ErrorMessageComponent
+                            errorMessage={this.props.errorMessage}
+                        />
+                    </Form>
+                </Content>
+            );
+        }
     }
 }
 
 const mapStateToProps = state => {
     return {
         errorMessage: state.articles.errorMessage,
-        name: state.auth.name
+        name: state.auth.name,
+        status: state.articles.status
     };
 };
 
@@ -126,6 +143,9 @@ const mapDispatchToProps = dispatch => {
                     date
                 )
             );
+        },
+        cleanServerStatus: () => {
+            return dispatch(articles.cleanServerStatus());
         }
     };
 };
